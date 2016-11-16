daf2c7873e4beeaf2370c37ad80f889cb39150c1
xiao-tian <luckloveyou95@gmail.com>
2016-11-16 11:37:36 +0800

    Update testJs.js

--- D3-master/testJs.js
+++ D3-master/testJs.js
@@ -1,60 +1,55 @@
 function testJs()
 {
 
-
 	var width  = 460;
 	var height = 268;
 	dataPath = 'https://github.com/xiao-tian/NTUB_Project/tree/master/D3-master/';
 	dataFile = 'table.csv';
 	dataUrl = dataPath + dataFile;
 	
-d3.csv(dataUrl, function(data) {
+d3.csv("dataUrl", function(data) {
 	console.log(data);
 	data.forEach(function(d) {
 		console.log(d.Open);
-		console.log(d.High);
 		console.log(d.Low);
 		console.log(d.Close);
 	});
+	
 	var maxHigh = d3.max(data, function(d) { return d.High; });
 	var ln = data.length;
 	var ctrl  = d3.select(".article").append("svg").attr("width", width).attr("height", height);
 
-	var linesOpen = d3.line().
-	x(function(d,i){ return i * (width/ln); }).
-	y(function(d){ return height - d.Open * (height/maxHigh); });
-
-	var linesHigh = d3.line().
+	var lines = d3.line().
 	x(function(d,i){ return i * (width/ln); }).
 	y(function(d){ return height - d.High * (height/maxHigh); });
+	ctrl.append("path").data([data]).
+	attr("class", "pathline").
+	attr("d", lines);
 
-	var linesLow = d3.line().
+	var line2 = d3.line().
 	x(function(d,i){ return i * (width/ln); }).
-	y(function(d){ return height - d.Low * (height/maxHigh); });
+	y(function(d){ return height - d.High * (height/maxHigh); });
+	ctrl.append("path").data([data]).
+	attr("class", "pathline").
+	attr("id", "pathline2").
+	attr("d", line2);
 
-	var linesClose = d3.line().
+    var line3 = d3.line().
 	x(function(d,i){ return i * (width/ln); }).
-	y(function(d){ return height - d.Close * (height/maxHigh); });
-
+	y(function(d){ return height - d.High * (height/maxHigh); });
 	ctrl.append("path").data([data]).
 	attr("class", "pathline").
-	attr("id", "pathlineOpen").
-	attr("d", linesOpen);
+	attr("id", "pathline3").
+	attr("d", line3);
 
+	var line4 = d3.line().
+	x(function(d,i){ return i * (width/ln); }).
+	y(function(d){ return height - d.High * (height/maxHigh); });
 	ctrl.append("path").data([data]).
 	attr("class", "pathline").
-	attr("id", "pathlineHigh").
-	attr("d", linesHigh);
-
-	ctrl.append("path").data([data]).
-	attr("class", "pathline").
-	attr("id", "pathlineLow").
-	attr("d", linesLow);
-
-	ctrl.append("path").data([data]).
-	attr("class", "pathline").
-	attr("id", "pathlineClose").
-	attr("d", linesClose);
+	attr("id", "pathline4").
+	attr("d", line4);
+	
 });
 
 }
