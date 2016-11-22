e6ee87d3db9ce4c15644657ce943fa1933be5c66
xiao-tian <luckloveyou95@gmail.com>
2016-11-23 02:54:24 +0800

    201611

--- D3-master/testJs.js
+++ D3-master/testJs.js
@@ -1,57 +1,59 @@
 function testJs()
 {
 
-dataPath = 'https://github.com/xiao-tian/NTUB_Project/tree/master/D3-master/';
+	var width  = 460;
+	var height = 268;
+	dataPath = 'https://github.com/xiao-tian/NTUB_Project/tree/master/D3-master/';
 	dataFile = 'table.csv';
 	dataUrl = dataPath + dataFile;
 	
-d3.csv(dataUrl, function(data) {
+	d3.csv(dataUrl, function(data) {
 	console.log(data);
 	data.forEach(function(d) {
 		console.log(d.Open);
-		console.log(d.High);
 		console.log(d.Low);
 		console.log(d.Close);
 	});
-	var maxHigh = d3.max(data, function(d) { return d.High; });
+
+	var maxy = d3.max(data, function(d) { return d.Open; });
+	var Highmaxy = d3.max(data, function(d) { return d.High; });
+	var Lowmaxy = d3.max(data, function(d) { return d.Low; });
+	var Closemaxy = d3.max(data, function(d) { return d.Close; });
+
 	var ln = data.length;
-	var ctrl  = d3.select(".newsletter").append("svg").attr("width", width).attr("height", height);
+	var ctrl  = d3.select(".artcile").append("svg").attr("width", width).attr("height", height);
 
-	var linesOpen = d3.line().
+	var lines = d3.line().
 	x(function(d,i){ return i * (width/ln); }).
-	y(function(d){ return height - d.Open * (height/maxHigh); });
-
-	var linesHigh = d3.line().
-	x(function(d,i){ return i * (width/ln); }).
-	y(function(d){ return height - d.High * (height/maxHigh); });
-
-	var linesLow = d3.line().
-	x(function(d,i){ return i * (width/ln); }).
-	y(function(d){ return height - d.Low * (height/maxHigh); });
-
-	var linesClose = d3.line().
-	x(function(d,i){ return i * (width/ln); }).
-	y(function(d){ return height - d.Close * (height/maxHigh); });
-
+	y(function(d){ return height-d.Open * (height/Highmaxy); });
 	ctrl.append("path").data([data]).
 	attr("class", "pathline").
-	attr("id", "pathlineOpen").
-	attr("d", linesOpen);
+	attr("d", lines);
 
+	var line2 = d3.line().
+	x(function(d,i){ return i * (width/ln); }).
+	y(function(d){ return height-d.High * (height/Highmaxy); });
 	ctrl.append("path").data([data]).
 	attr("class", "pathline").
-	attr("id", "pathlineHigh").
-	attr("d", linesHigh);
+	attr("id", "pathline2").
+	attr("d", line2);
 
+    var line3 = d3.line().
+	x(function(d,i){ return i * (width/ln); }).
+	y(function(d){ return height-d.Low * (height/Highmaxy); });
 	ctrl.append("path").data([data]).
 	attr("class", "pathline").
-	attr("id", "pathlineLow").
-	attr("d", linesLow);
+	attr("id", "pathline3").
+	attr("d", line3);
 
+	var line4 = d3.line().
+	x(function(d,i){ return i * (width/ln); }).
+	y(function(d){ return height-d.Close * (height/Highmaxy); });
 	ctrl.append("path").data([data]).
 	attr("class", "pathline").
-	attr("id", "pathlineClose").
-	attr("d", linesClose);
+	attr("id", "pathline4").
+	attr("d", line4);
+	
 });
 
 }
